9ef571113c56a762048fb9aec763c35cb5442815
felix1251 <felixabacajen17@gmail.com>
2025-09-20 13:25:39 +0800

    feat: frontend types

---
+++ frontend/src/types/product.type.ts
@@ -0,0 +1,6 @@
+export type Product = {
+  id: number;
+  base_price: number;
+  code: string;
+  name: string;
+};
---
+++ frontend/src/types/product_promotion.type.ts
@@ -0,0 +1,5 @@
+export type ProductPromotion = {
+  id: number;
+  product_id: number;
+  promotion_rule_id: number;
+};
